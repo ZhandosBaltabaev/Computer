4074fdb32628b37d864bf599abe0f4dcfdd08ecf
ZhandosBaltabaev <90600100+ZhandosBaltabaev@users.noreply.github.com>
2021-09-23 08:07:52 +0300

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,13 @@
+number=5#int
+
+digit=-4.54356876
+word="Result:"#string
+boolean=True#bool
+
+str_num='5'#string
+#print(word+str(digit))
+print(word+str(number+int(str_num)))
+del number
+
+number=7
+print("Result:", number)
---
+++ 5.py
@@ -0,0 +1,16 @@
+num1=int(input("Add first number:"))
+
+num2=int(input("Add second number:"))
+
+num1*5
+print("Result:", num1+num2)
+print("Result:", num1-num2)
+print("Result:", num1/num2)
+print("Result:", num1*num2)
+print("Result:", num1**num2)
+print("Result:", num1//num2)
+
+word="Hi"
+print(word*2)
+
+word=True
